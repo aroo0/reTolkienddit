d5ec54b9b47bcdbf857639679fa859ebd6cb9632
aroogit commit -m First <arek.dec@gmail.com>
2023-03-12 18:08:38 +0100

    added subredits panel

--- src/features/acounts/acounts.js
+++ src/features/acounts/acounts.js
@@ -1,12 +1,30 @@
+import avatar from '../../data/communityIcon_wmmxgfx6xrf01.png'
+
 export default function Acounts() {
     return (
-        <aside class='main-content__accounts-list'>
-          <div class='accounts-list__wrapper'>
-          <h2 class='accounts-list__title'>Subreddits</h2>
-          <ul>
-            <li>Tolkien</li>
-            <li>lotr</li>
-            <li>tolkienbooks</li>
+        <aside class='main-content__accounts-panel'>
+          <div class='accounts-panel__wrapper'>
+          <h2 class='accounts-panel__title'>Subreddits</h2>
+          <ul class='accounts-list'>
+            <li>
+              <button class='account'>
+                <img class='account__avatar' src={avatar} alt="user avatar" />
+                Tolkien
+              </button>
+            </li>
+            <li>
+              <button class='account'>
+                <img class='account__avatar' src={avatar} alt="user avatar" />
+                TolkienMemes
+              </button>
+            </li>
+            <li>
+              <button class='account'>
+                <img class='account__avatar' src={avatar} alt="user avatar" />
+                YourLOTRquotes
+              </button>
+            </li>
+
           </ul>
         </div>
       </aside>
