23d0bde9863fd086cc4c9ccb819ba04860b1ccf1
aroogit commit -m First <arek.dec@gmail.com>
2023-05-11 11:59:23 +0200

    centring spinner

--- src/components/loading/loading.js
+++ src/components/loading/loading.js
@@ -1,9 +1,9 @@
 export function Loading() {
     return (
         <div className='post' >
-        <div className="loading-container">
-        <div className="loading-spinner" />
-        <div className="loading-text">Loading...</div>
+            <div className="loading-container">
+            <div className="loading-spinner" />
+            <div className="loading-text">Loading...</div>
       </div>
 </div>
     )
