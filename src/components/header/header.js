b7bc08bcd470566c00510a85007c0b3282696404
aroogit commit -m First <arek.dec@gmail.com>
2023-05-11 12:17:03 +0200

    change class to className

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,9 +3,9 @@ import SearchBar from "../search/searchBar"
 
 export function MyHeader() {
     return (
-        <header class='header'>
-        <h1 class='header__logo'>re<span class='header__logo-tolkien'>Tolkien</span>ddit</h1>
-        <SearchBar class='header__search-bar' />
+        <header className='header'>
+        <h1 className='header__logo'>re<span className='header__logo-tolkien'>Tolkien</span>ddit</h1>
+        <SearchBar className='header__search-bar' />
       </header>
     )
 
